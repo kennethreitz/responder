90083f945e952d2b07239919b94859bf0ded872a
Kenneth Reitz <me@kennethreitz.org>
2018-10-11 13:18:40 -0400

    cleanup

--- t.py
+++
@@ -1,9 +0,0 @@
-import requests
-from wsgiadapter import WSGIAdapter
-
-from app import api
-
-s = requests.Session()
-s.mount("http://staging/", WSGIAdapter(api))
-r = s.get("http://staging/")
-print(r)
