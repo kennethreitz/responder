6cf256cc052cf7aec58615b497263de05f27b316
Kenneth Reitz <me@kennethreitz.org>
2018-10-12 16:48:41 -0400

    memoize routes

--- responder/routes.py
+++ responder/routes.py
@@ -1,6 +1,17 @@
 from parse import parse, search
 
 
+def memoize(f):
+    memo = {}
+
+    def helper(self, s):
+        if s not in memo:
+            memo[s] = f(self, s)
+        return memo[s]
+
+    return helper
+
+
 class Route:
     def __init__(self, route, endpoint):
         self.route = route
@@ -21,6 +32,7 @@ class Route:
     def has_parameters(self):
         return all([("{" in self.route), ("}" in self.route)])
 
+    @memoize
     def does_match(self, s):
         if s == self.route:
             return True
@@ -28,6 +40,7 @@ class Route:
         named = self.incoming_matches(s)
         return bool(len(named))
 
+    @memoize
     def incoming_matches(self, s):
         results = parse(self.route, s)
         return results.named if results else {}
