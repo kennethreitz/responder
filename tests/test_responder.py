261601230af3a9abc7a1219760e56ead75adc0d5
Kenneth Reitz <me@kennethreitz.org>
2018-10-14 06:21:41 -0400

    better tests

--- tests/test_responder.py
+++ tests/test_responder.py
@@ -1,49 +1,9 @@
-import graphene
 import pytest
 import responder
 import yaml
 
 
-@pytest.fixture
-def api():
-    return responder.API()
-
-
-@pytest.fixture
-def session(api):
-    return api.session()
-
-
-@pytest.fixture
-def url():
-    def url_for(s):
-        return f"http://;{s}"
-
-    return url_for
-
-
-@pytest.fixture
-def flask():
-    from flask import Flask
-
-    app = Flask(__name__)
-
-    @app.route("/")
-    def hello():
-        return "Hello World!"
-
-    return app
-
-
-@pytest.fixture
-def schema():
-    class Query(graphene.ObjectType):
-        hello = graphene.String(name=graphene.String(default_value="stranger"))
-
-        def resolve_hello(self, info, name):
-            return f"Hello {name}"
-
-    return graphene.Schema(query=Query)
+from .fixtures import *
 
 
 def test_api_basic_route(api):
