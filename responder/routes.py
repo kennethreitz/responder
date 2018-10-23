586fad76460ebbce20320dbb1a0d7ae0259bc826
taoufik07 <abacidtaoufik@gmail.com>
2018-10-23 21:19:57 +0100

    Fix CBV

--- responder/routes.py
+++ responder/routes.py
@@ -73,8 +73,9 @@ class Route:
     def is_class_based(self):
         return hasattr(self.endpoint, "__class__")
 
+    @property    
     def is_function(self):
         routed = hasattr(self.endpoint, "is_routed")
         code = hasattr(self.endpoint, "__code__")
         kwdefaults = hasattr(self.endpoint, "__kwdefaults__")
-        return all((routed, code, kwdefaults))
+        return all((callable(self.endpoint), code, kwdefaults))
