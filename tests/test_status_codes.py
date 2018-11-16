67d8a3be98c878f272302d91f0def639bd9c862d
Tessei Kameyama <kamenoko315@ruri.waseda.jp>
2018-11-16 20:23:09 +0900

    [fix] typo

--- tests/test_status_codes.py
+++ tests/test_status_codes.py
@@ -8,7 +8,7 @@ from responder import status_codes
         pytest.param(101, True, id="Normal 101"),
         pytest.param(199, True, id="Not actual status code but within 100"),
         pytest.param(0, False, id="Zero case (below 100)"),
-        pytest.param(200, False, id="Above 100")
+        pytest.param(200, False, id="Above 100"),
     ],
 )
 def test_is_100(status_code, expected):
@@ -21,7 +21,7 @@ def test_is_100(status_code, expected):
         pytest.param(201, True, id="Normal 201"),
         pytest.param(299, True, id="Not actual status code but within 200"),
         pytest.param(0, False, id="Zero case (below 200)"),
-        pytest.param(300, False, id="Above 200")
+        pytest.param(300, False, id="Above 200"),
     ],
 )
 def test_is_200(status_code, expected):
@@ -34,7 +34,7 @@ def test_is_200(status_code, expected):
         pytest.param(301, True, id="Normal 301"),
         pytest.param(399, True, id="Not actual status code but within 300"),
         pytest.param(0, False, id="Zero case (below 300)"),
-        pytest.param(400, False, id="Above 300")
+        pytest.param(400, False, id="Above 300"),
     ],
 )
 def test_is_300(status_code, expected):
@@ -47,7 +47,7 @@ def test_is_300(status_code, expected):
         pytest.param(401, True, id="Normal 401"),
         pytest.param(499, True, id="Not actual status code but within 400"),
         pytest.param(0, False, id="Zero case (below 400)"),
-        pytest.param(500, False, id="Above 400")
+        pytest.param(500, False, id="Above 400"),
     ],
 )
 def test_is_400(status_code, expected):
@@ -57,10 +57,10 @@ def test_is_400(status_code, expected):
 @pytest.mark.parametrize(
     "status_code, expected",
     [
-        pytest.param(501, True, id="Normal 401"),
-        pytest.param(599, True, id="Not actual status code but within 400"),
-        pytest.param(0, False, id="Zero case (below 400)"),
-        pytest.param(600, False, id="Above 500")
+        pytest.param(501, True, id="Normal 501"),
+        pytest.param(599, True, id="Not actual status code but within 500"),
+        pytest.param(0, False, id="Zero case (below 500)"),
+        pytest.param(600, False, id="Above 500"),
     ],
 )
 def test_is_500(status_code, expected):
