0855d1a378d4b3f134e8ecb5ff8e6218f3c04e1b
taoufik <abacidtaoufik@gmail.com>
2019-12-15 16:12:43 +0100

    Update requirements to support python3.8

--- setup.py
+++ setup.py
@@ -23,14 +23,13 @@ if sys.argv[-1] == "publish":
 
 required = [
     "starlette==0.12.*",
-    "uvicorn>=0.7, <0.9",
+    "uvicorn==0.10.*",
     "aiofiles",
     "pyyaml",
     "requests",
     "graphene<3.0",
     "graphql-server-core>=1.1",
     "jinja2",
-    "uvloop; sys_platform != 'win32' and sys_platform != 'cygwin' and sys_platform != 'cli'",
     "rfc3986",
     "python-multipart",
     "chardet",
