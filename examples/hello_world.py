25050d183872f752debb3c37aa0a31d2e542b01b
Kenneth Reitz <me@kennethreitz.org>
2023-11-12 06:31:21 -0500

    hello world

---
+++ examples/hello_world.py
@@ -0,0 +1,10 @@
+import responder
+
+api = responder.API()
+
+@api.route("/{greeting}")
+async def greet_world(req, resp, *, greeting):
+    resp.text = f"{greeting}, world!"
+
+if __name__ == '__main__':
+    api.run()
