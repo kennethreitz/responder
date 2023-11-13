ebe4265f158603c428cd2a70854c167e5ed216f9
Kenneth Reitz <me@kennethreitz.org>
2023-11-12 23:44:53 -0500

    better hello_form

--- examples/hello_form.py
+++ examples/hello_form.py
@@ -1,37 +1,29 @@
-import time
-import responder
+from typing import List, Optional
 
+import responder
 from pydantic import BaseModel
+
 
 api = responder.API()
 
 # responder.Model
 class Item(BaseModel):
-    # file: dict
-    hello: list
+    hello: List
 
 @api.route("/file")
 async def receive_incoming(req, resp):
 
-    @api.background.task
-    def process_data(data):
-        """Just sleeps for three seconds, as a demo."""
-        time.sleep(3)
-
-
     # Parse the incoming data as form-encoded.
     # Note: 'json' and 'yaml' formats are also automatically supported.
     data = await req.data(Item)
+    
+    # Print the incoming data to stdout.
     print(data)
 
-    # Process the data (in the background).
-    process_data(data)
-    resp.model = Item
     # Immediately respond that upload was successful.
     resp.media = {'success': True}
 
 
 # Let's make an HTTP request to the server, to test it out.'}
 r = api.requests.post('http://;/file', data={'hello': 'world'})
-
 print(r.text)
