f547be6b48a1fe0e4b79ef6434bd4271460ad5ff
Kenneth Reitz <me@kennethreitz.org>
2023-11-13 00:52:15 -0500

    even cleanerer

--- examples/hello_form.py
+++ examples/hello_form.py
@@ -1,19 +1,15 @@
-from typing import List, Optional
-
 import responder
 from pydantic import BaseModel
 
 
-api = responder.API()
-
-# responder.Model
 class ItemModel(BaseModel):
-    hello2: List
+    hello2: list
+
+api = responder.API()
 
 @api.route("/file")
 async def receive_incoming(req, resp):
     print(await req.validate(ItemModel))
-
 
 # Let's make an HTTP request to the server, to test it out.'}
 r = api.requests.post('http://;/file', data={'hello': 'world'})
