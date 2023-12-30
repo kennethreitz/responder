1e09600ab9800e5d3af03505b3fafd486b7943dc
Kenneth Reitz <me@kennethreitz.org>
2023-12-30 07:11:29 -0500

    Refactor code to use pydantic for FortuneModel and extract fortune generation to separate function

--- examples/hello_fortune.py
+++ examples/hello_fortune.py
@@ -1,23 +1,28 @@
 import subprocess
 
 import responder
+from pydantic import BaseModel
 
 api = responder.API()
 
 
+class FortuneModel(BaseModel):
+    fortune: str
+
+
+def generate_fortune():
+    fortune = subprocess.check_output(["fortune"]).decode().strip()
+    return FortuneModel(fortune=fortune)
+
+
+@api.ensure(FortuneModel)
 @api.route("/fortune")
 class GreetingResource:
-    def on_request(self, req, resp):  # or on_get...
-        resp.headers.update({"X-Life": "42"})
-
     def on_get(self, req, resp):
-        resp.headers.update({"X-ArtificialLife": "400"})
-
-        fortune = subprocess.check_output(["fortune"]).decode()
-        resp.media = {"fortune": fortune}
+        fortune = generate_fortune()
+        resp.media = {"fortune": fortune.fortune}
 
 
 # Let's make an HTTP request to the server, to test it out.'}
 r = api.requests.get("http://;/fortune")
 print(r.text)
-print(r.headers)
