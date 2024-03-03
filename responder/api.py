734f02f6e13f01a7fabd37437823f4b292c0f864
Tabot Kevin <tabot.kevin@andela.com>
2024-03-03 17:39:04 +0100

    Added decorator to capture other responses of an endpoint for documentation

--- responder/api.py
+++ responder/api.py
@@ -6,9 +6,9 @@ import marshmallow
 import pydantic
 import uvicorn
 from sqlalchemy.orm import DeclarativeBase, Query
+from starlette.exceptions import ExceptionMiddleware
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.errors import ServerErrorMiddleware
-from starlette.middleware.exceptions import ExceptionMiddleware
 from starlette.middleware.gzip import GZipMiddleware
 from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
 from starlette.middleware.sessions import SessionMiddleware
@@ -322,6 +322,44 @@ class API:
         :param **kwargs: Data to pass into the template.
         """
         return self.templates.render_string(source, *args, **kwargs)
+
+    def _annotate(self, f, **kwargs):
+        """Utilized to store essential route details for later inclusion in the
+        OpenAPI documentation of the route."""
+
+        if not hasattr(f, "_spec"):
+            f._spec = {}
+        for key, value in kwargs.items():
+            f._spec[key] = value
+
+    def expect(self, responses):
+        """A decorator that receives key pair values of status_codes and descriptions
+        for other responses expected the by endpoint. This decorator is only
+        used for OpenAPI documentation.
+
+        :params codes: e.g {401: 'Invalid access or refresh token', 404: 'Item not found'}
+
+
+        Usage::
+            api = responder.API()
+
+            @api.route("/create")
+            @api.trust(ItemCreate)
+            @api.expect(
+                {
+                    401: "Invalid access or refresh token",
+                    403: "Please verify your account",
+                }
+            )
+            async def create_items(req, resp):
+                resp.text = "Item created"
+        """
+
+        def decorator(f):
+            self._annotate(f, expect=responses)
+            return f
+
+        return decorator
 
     def trust(self, schema):
         """A decorator for parsing and validating input schema.
